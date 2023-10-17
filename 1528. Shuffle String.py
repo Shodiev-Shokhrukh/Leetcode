011a82859b9e295cad10d179320f419234b7bba5
Shodiev-Shokhrukh <shodiev.shokhrukh98@gmail.com>
2023-10-17 21:50:07 +0500

    1678, 2114, 1528, 1816, 1662 problems solved

---
+++ 1528. Shuffle String.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        r=""
+        for i in range(len(s)):
+            r=r+s[indices.index(i)]
+        return r
---
+++ 2114. Maximum Number of Words Found in Sentences.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        max_words = 0
+    
+        for sentence in sentences:
+            words = sentence.split()  
+            word_count = len(words)   
+            max_words = max(max_words, word_count)  
+        
+        return max_words
