6da97f299c0e6c16c749b60cc4b71f7e5bc4342a
Shodiev-Shokhrukh <shodiev.shokhrukh98@gmail.com>
2023-10-27 12:02:49 +0500

    202, 1941, 2678th problems solved

---
+++ Strings/2678. Number of Senior Citizens.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        count = 0
+        for detail in details:
+            if int(detail[-4:-2]) > 60:
+                count +=1
+        return count
